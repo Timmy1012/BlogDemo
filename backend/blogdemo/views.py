cfd81f8faaa2a43f34c8efd47f1b4ad729055f0e
Timmy1012 <tim.van.der.hoeven@hotmail.com>
2025-11-09 10:33:45 +0100

    Add put method to perform an update on an article

--- backend/blogdemo/views.py
+++ backend/blogdemo/views.py
@@ -7,3 +7,11 @@ from .models import Article
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
+    
+    # Define a PUT method that handles the update action
+    def put(self, request, *args, **kwargs):
+        """
+        Handle PUT requests for updating an article.
+        This method provides the same functionality as the default update method.
+        """
+        return super().update(request, *args, **kwargs)
